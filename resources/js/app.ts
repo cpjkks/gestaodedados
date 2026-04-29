262ce7b9ab956fc94ab69345183b35a6fd9dde9a
João Peccini <jppeccini2@gmail.com>
2026-04-29 08:09:37 -0300

    Atualizações

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,10 +5,17 @@ import AuthLayout from '@/layouts/AuthLayout.vue';
 import SettingsLayout from '@/layouts/settings/Layout.vue';
 import { initializeFlashToast } from '@/lib/flashToast';
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css';
+
+import { createApp, h } from 'vue';
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
+
     layout: (name) => {
         switch (true) {
             case name === 'Welcome':
@@ -21,13 +28,30 @@ createInertiaApp({
                 return AppLayout;
         }
     },
+
     progress: {
         color: '#4B5563',
     },
+
+    setup({ el, App, props, plugin }) {
+        const app = createApp({
+            render: () => h(App, props)
+        });
+
+        app.use(plugin);
+
+        app.use(PrimeVue, {
+            theme: {
+                preset: Aura
+            }
+        });
+
+        app.mount(el!);
+    }
 });
 
-// This will set light / dark mode on page load...
+// Theme
 initializeTheme();
 
-// This will listen for flash toast data from the server...
+// Flash messages
 initializeFlashToast();
--- resources/js/pages/TipoDeTurno/Index.vue
+++ resources/js/pages/TipoDeTurno/Index.vue
@@ -1,21 +1,49 @@
 <script setup>
-import AppLayout from '@/layouts/AppLayout.vue';
-import { Head, Link } from '@inertiajs/vue3';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Button from 'primevue/button';
+import { Link } from '@inertiajs/vue3';
 
 // Recebe os dados que o Controller mandou (o array 'cursos')
 defineProps({
-    cursos: Array,
+    tipo_de_turno: Array,
 });
 
 // Função para confirmar a exclusão com segurança
 const confirmarExclusao = (event) => {
-    if (!confirm('Tem certeza que deseja excluir este curso?')) {
+    if (!confirm('Tem certeza que deseja excluir este Tipo de Turno?')) {
         event.preventDefault();
     }
 };
 </script>
 
 <template>
-    <Head title="Tipos De Turno" />
+    
+    <div class="flex justify-end mb-4">
+        <Link href="/tipo_de_turno/create">
+            <Button label="Novo" icon="pi pi-plus" severity="success" />
+        </Link> 
+    </div>
 
+    <DataTable :value="tipo_de_turno" paginator :rows="5">
+
+        <Column field="nome" header="Nome" />
+
+        <Column header="Ações">
+            <template #body="slotProps">
+                <div class="flex gap-2">
+
+                    <Link :href="route('tipo_de_turno.show', slotProps.data.id)">
+                        <Button icon="pi pi-eye" severity="info" />
+                    </Link>
+
+                    <Link :href="route('tipo_de_turno.edit', slotProps.data.id)">
+                        <Button icon="pi pi-pencil" severity="warning" />
+                    </Link>
+
+                </div>
+            </template>
+        </Column>
+
+    </DataTable>
 </template>
